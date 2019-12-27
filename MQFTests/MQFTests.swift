a19c0d60553f2cb299ab987f6d0df8c58d7a89aa
cbrech <cbrechbuhl@gmail.com>
2019-12-27 01:49:28 -0500

    Added Unit Testing to go through all presets to ensure they work

--- MQFTests/MQFTests.swift
+++ MQFTests/MQFTests.swift
@@ -21,6 +21,75 @@ class MQFTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    /// Iterates through every preset to make sure it loads properly
+    /// Then goes through each question to make sure it has at least 2 possible answers
+    /// Submits the correct answer for each question to ensure user can get credit if they choose the correct one
+    func testAllPresets(){
+        DataManager.shared.load()
+        let bases = DataManager.shared.availableBases
+        XCTAssert(bases.count > 0, "No bases found")
+        
+        var presets = [MQFPreset]()
+        for base in bases{
+            for preset in base.presets{
+                presets.append(preset)
+            }
+        }
+        XCTAssert(presets.count > 0, "No presets found")
+        
+        for preset in presets{ // For each preset in data set
+            var activeMQFs = [MQFData]()
+            for mqf in preset.mqfs{
+                activeMQFs.append(mqf)
+            }
+            
+            let quizSession = QKSession.default
+            var superQuiz = QKQuiz()
+            var testTotalQuestions = 0
+            for mqf in activeMQFs{
+                testTotalQuestions += mqf.testNum
+            }
+
+            for mqf in activeMQFs{
+                let name = mqf.filename.replacingOccurrences(of: ".json", with: "")
+                guard let path = Bundle.main.path(forResource: name, ofType: "json") else {
+                    return
+                }
+                if let quiz = QKQuiz(loadFromJSONFile: path) {
+                    XCTAssert(quiz.orderedQuestions.count > 0, "No questions found for MQF \(name)")
+                    superQuiz.appendQuiz(quiz: quiz, limit:0)
+                }
+            }
+            quizSession.load(quiz: superQuiz)
+            XCTAssert(superQuiz.orderedQuestions.count > 0, "No questions found")
+       
+            for question in superQuiz.orderedQuestions{
+                XCTAssert(question.responses.count > 1, "Not enough responses found for \(question.question)")
+            }
+     
+            do {
+                try quizSession.start()
+            } catch {
+                fatalError("Quiz started without quiz set on the session")
+            }
+            var activeQuestion:QKQuestion? = nil
+            while let question = quizSession.nextQuestion(after: activeQuestion){
+                XCTAssert(question.responses.count > 1, "No question responses found for \(question.question)")
+                quizSession.submit(response: question.correctResponse, for: question)
+                
+                activeQuestion = question
+            }
+            
+            XCTAssertEqual(quizSession.responseCount, superQuiz.orderedQuestions.count, "Different number of answers than questions")
+            XCTAssertEqual(quizSession.score, superQuiz.orderedQuestions.count, "Score was not 100")
+            quizSession.score
+            
+
+        }
+        
+       }
+    
+    
     func testCreatePreset(){
            let jsonString = "{\"name\":\"437/315 AW Pilot Test\",\"id\":\"KCHS-Pilot-Airland\",\"positions\":[\"Pilot\"],\"mqfs\":[{\"testNum\":30,\"file\":\"c17-Pilot-1nov\"},{\"testNum\":5,\"file\":\"c17-KCHS-Pilot\"}],\"testTotal\":35}"
            let json = JSON(parseJSON: jsonString)
