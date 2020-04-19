406bc833eae7c326cf504d7f22806d9e335cbfcb
christian.brechbuhl <cbrechbuhl@gmail.com>
2020-04-19 18:49:51 -0400

    Commented out unused code

--- MQF/Extensions and Helpers/extensions.swift
+++ MQF/Extensions and Helpers/extensions.swift
@@ -8,36 +8,36 @@ import Foundation
 import UIKit
 extension UIButton {
     
-    func centerVertically(padding: CGFloat = 6.0) {
-        guard
-            let imageViewSize = self.imageView?.frame.size,
-            let titleLabelSize = self.titleLabel?.frame.size else {
-                return
-        }
-        
-        let totalHeight = imageViewSize.height + titleLabelSize.height + padding
-        
-        self.imageEdgeInsets = UIEdgeInsets(
-            top: -(totalHeight - imageViewSize.height),
-            left: 0.0,
-            bottom: 0.0,
-            right: -titleLabelSize.width
-        )
-        
-        self.titleEdgeInsets = UIEdgeInsets(
-            top: 0.0,
-            left: -imageViewSize.width,
-            bottom: -(totalHeight - titleLabelSize.height),
-            right: 0.0
-        )
-        
-        self.contentEdgeInsets = UIEdgeInsets(
-            top: 0.0,
-            left: 0.0,
-            bottom: titleLabelSize.height,
-            right: 0.0
-        )
-    }
+//    func centerVertically(padding: CGFloat = 6.0) {
+//        guard
+//            let imageViewSize = self.imageView?.frame.size,
+//            let titleLabelSize = self.titleLabel?.frame.size else {
+//                return
+//        }
+//        
+//        let totalHeight = imageViewSize.height + titleLabelSize.height + padding
+//        
+//        self.imageEdgeInsets = UIEdgeInsets(
+//            top: -(totalHeight - imageViewSize.height),
+//            left: 0.0,
+//            bottom: 0.0,
+//            right: -titleLabelSize.width
+//        )
+//        
+//        self.titleEdgeInsets = UIEdgeInsets(
+//            top: 0.0,
+//            left: -imageViewSize.width,
+//            bottom: -(totalHeight - titleLabelSize.height),
+//            right: 0.0
+//        )
+//        
+//        self.contentEdgeInsets = UIEdgeInsets(
+//            top: 0.0,
+//            left: 0.0,
+//            bottom: titleLabelSize.height,
+//            right: 0.0
+//        )
+//    }
     func alignImageAndTitleVertically(padding: CGFloat = 6.0) {
         let imageSize = self.imageView!.frame.size
         let titleSize = self.titleLabel!.frame.size
