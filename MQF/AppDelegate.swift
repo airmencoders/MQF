8faadb3117337936b52b38e603c33cad86d98298
Christian <christian.brechbuhl@afwerx.af.mil>
2020-10-05 16:36:19 -0400

    Banned 3rd Party Keyboards

--- MQF/AppDelegate.swift
+++ MQF/AppDelegate.swift
@@ -59,6 +59,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    func application(_ application: UIApplication, shouldAllowExtensionPointIdentifier extensionPointIdentifier: UIApplication.ExtensionPointIdentifier) -> Bool {
+        if(extensionPointIdentifier == UIApplication.ExtensionPointIdentifier.keyboard){
+            return false;
+        }
+        return true;
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
