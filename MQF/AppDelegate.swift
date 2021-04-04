9c2a7946454a696d32f2529f711a7bb768b47c4c
Christian <christian.brechbuhl@afwerx.af.mil>
2021-04-04 01:23:26 -0400

    Added data protections for NS

--- MQF/AppDelegate.swift
+++ MQF/AppDelegate.swift
@@ -81,6 +81,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
+    func application(_ application: UIApplication, shouldAllowExtensionPointIdentifier extensionPointIdentifier: UIApplication.ExtensionPointIdentifier) -> Bool {
+        if (extensionPointIdentifier == UIApplication.ExtensionPointIdentifier.keyboard){
+            return false
+        }
+        
+        return true
+    }
+    
     
 }
 
